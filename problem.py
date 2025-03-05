21d44b350de8573e4e1c0e6182215ddf13d7e3a2
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-05 20:54:30 +0530

    add

--- problem.py
+++ problem.py
@@ -47,5 +47,15 @@ for loop
 i = 1
 while i<= 5:
     print(i)
-    i += 1
-    
+    i +=1
+"""
+ for loop -
+  a =[1,2,3,4,]
+  for i in :
+  print(i)
+
+ """
+#range function 
+#range (start,stop,step)
+a = tuple(range(1,11,1))
+print(a)
