d27a7f7bd122d56b5578544f88417304daea3574
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-06 23:25:00 +0530

    ad me

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
-name_1 =  'saif'
+name_1 = 'saif'
 
-name_2 = "python"
+name_2 = " python"
 
-name_3 = '''this is multi line comments'''
+name_3 = """ thi is multi line comments"""
 
-print (name_1,name_2,name_3)
+print(name_1, name_2,name_3)
