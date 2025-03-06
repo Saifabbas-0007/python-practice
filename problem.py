b3285d069759659e3c5959032383e130cc16c1e5
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-06 23:31:00 +0530

    add

--- problem.py
+++ problem.py
@@ -86,3 +86,4 @@ for i in range (1,11):
             if i == skip:
                 continue
             print(i)
+            
--- string.py
+++
@@ -1,7 +0,0 @@
-name_1 = 'saif'
-
-name_2 = " python"
-
-name_3 = """ thi is multi line comments"""
-
-print(name_1, name_2,name_3)
