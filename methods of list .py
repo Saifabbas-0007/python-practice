0e8a629a006cdc35b6121b2a4ac77fb405cd0da7
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-07 23:04:15 +0530

    list method

--- methods of list .py
+++ methods of list .py
@@ -3,3 +3,21 @@
 a = [1,2,3]
 a.append(4)
 print(a)
+
+#extend : 
+a = [1,2,3]
+b = [4,5,6]
+a.extend(b)
+print(a)
+
+#insert method: insert elements where you want without removing or deleting the elements 
+
+list_1 = [1,2,3,4]
+list_1.insert(3,"hello coder")
+print(list_1) 
+
+#remove method :to delete the particular elments
+
+a = [1,2,3,4]
+a.remove(3)
+print(a)
