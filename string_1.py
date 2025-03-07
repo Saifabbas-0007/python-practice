12c594e787d748d0d98b624d75a3e32170804fda
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-07 13:40:56 +0530

    add me

--- string_1.py
+++ string_1.py
@@ -4,4 +4,22 @@ name_2 = "abbas"
 
 name_3 = """ this is multi line comments """
 
-print(name_1,name_2,name_3)
+print(name_1,name_2,name_3) 
+"""
+string -immutable
+follows indexing
+p 0
+y 1 
+t 2
+h 3
+o 4
+n 5
+
+"""
+a = "python"
+print( a[-1])
+'''
++ve indexing starts from 0
+-ve indexing starts from -1 
+
+'''
