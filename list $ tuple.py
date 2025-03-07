11297916087549ac8f8a7ab40193f21ca723a04e
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-07 17:31:29 +0530

    add me

--- list $ tuple.py
+++ list $ tuple.py
@@ -4,5 +4,19 @@
 2- mutable
  3- dynamic 
  4- heterogeneous  '''
+# 1 ways using sqaure bracket
+
 my_list = [1,2,3,10.56,True,"hello"]
 print(my_list)  
+
+#2 ways using  list constructor
+
+my_list = list((1,2,3,45.67, True,"hello"))
+print(my_list)
+
+#list comprehension and range 
+#update in list
+lst = [1, 2, 3, 4, 5]
+print(f"before update{lst}")
+lst [2] = "hello"
+print(f"after uodate{lst}")
