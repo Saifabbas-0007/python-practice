8a580c0db692d2715227c5f4634f1d971db5f1e2
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-11 00:13:53 +0530

    add me

--- dictionary.py
+++ dictionary.py
@@ -4,9 +4,44 @@ characteristic
 1- pair = key :value
 1 unordered 3.7version, but now it is ordered
 3- mutable
- 4- dynamic
-"""
+4- dynamic
+5- key namealways unique 
+ """
 my_dict ={ 
     "name":"saif","age":23,"marks":62.67,100:"abcd"
 }
 print(my_dict)
+my_dict ={
+    "marks":[1,2,3,'saif']
+}
+print(my_dict)
+
+#adding new value in dict
+my_dict ={
+    'fruites':['kela','apple','orange'],'categor':'fruites'
+} 
+print(my_dict)
+my_dict['prices'] = 100
+print(my_dict)
+
+#updation
+my_dict ={
+    'name':'saif','language':'python','version':3.9 #if key is same the value is update
+}
+print(my_dict)
+my_dict['version'] = 4.0
+print(my_dict) 
+my_dict ={
+    'name':'saif','language':'python','version':3.9 #if key is diff it add that value  
+}
+print(my_dict)
+my_dict['updated_version'] = 4.0
+print(my_dict) 
+
+#deleting the value from dict
+my_dict ={
+    'name':'saif','language':'python','version':3.9,'use case':['ai','ml','da',]
+}
+print(my_dict)
+del my_dict['version']
+print(my_dict) 
