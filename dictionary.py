588a3558370ef6dc5d7053dd5be3da28b4b95a4c
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-10 23:53:11 +0530

    add me

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+"""
+dictionary {}
+characteristic
+1- pair = key :value
+1 unordered 3.7version, but now it is ordered
+3- mutable
+ 4- dynamic
+"""
+my_dict ={ 
+    "name":"saif","age":23,"marks":62.67,100:"abcd"
+}
+print(my_dict)
