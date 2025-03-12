a4a86eb11e37650b77d02ab0e873a5ead1a3d650
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-13 00:36:33 +0530

    add me

--- fuction.py
+++ fuction.py
@@ -45,3 +45,43 @@ function name must be meaninful
 mast be small in lenght
 avoid global variable
 '''
+#local variable :whic can be access inside the function
+def msg():
+    choice = "i love coding"
+    print(choice)
+msg()   
+
+#global variable :which can be access outside the function
+def msg():
+    print("inside the function",choice)
+choice = "I Love coding"
+msg()  
+
+#decorator
+'''example burger - function
+extra cheese-extra feature
+main function > function adding 
+without changing the main function code
+'''
+def my_decorator(func):
+    def wrapper():
+        print("hello coder")
+        func()
+        print("hello python coders")
+    return wrapper
+@my_decorator
+def say_hello():
+    print("hii")
+say_hello()
+
+#generators  :to generate a sequence of value over a time function is used as "yield"
+def count_down(num):
+    while num >0:
+        yield num
+        num -= 1
+for number in count_down(5):
+    print(number)
+
+#lambda function
+add_num =lambda a:a+10
+print(add_num(10))
