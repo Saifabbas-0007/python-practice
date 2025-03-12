158650c063588024287d4771e12b905615f723f4
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-12 15:33:28 +0530

    add me

--- dictionary.py
+++ dictionary.py
@@ -45,3 +45,76 @@ my_dict ={
 print(my_dict)
 del my_dict['version']
 print(my_dict) 
+
+#get() method 
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+age = my_dict.get('age')
+print(age)
+
+#keys()
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+keys = my_dict.keys()
+print(list(keys))
+
+#values()
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+value = my_dict.values()
+print(list(value))  
+
+#items(): to call keys and value elements
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+all_items = my_dict.items()
+print(list(all_items))
+ 
+#popped()method
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+popped = my_dict.pop('age')
+print(popped)
+print(my_dict)
+ 
+ #popped items():to delete the last elements from the dict
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+popped_i = my_dict.popitem()
+print(popped_i)
+print(my_dict)
+
+#clear():to delete the total elements of dict
+my_dict = {
+    'name':'saif','age':23,'salary':25000.00
+}
+clear_i = my_dict.clear()
+print(my_dict)
+print(clear_i)
+
+#dict comprehension 
+squares = {a: a *a for a in range(1,6)}
+print(squares)
+
+ #nested dict:
+programming_language = {
+    "python":{"name":"python","versio":4.9,"use_case":[ 'ai,ml,webdev']}
+    ,"java":{"name":"java","versio":3.9,"use_case":[ 'ai,ml,webdev']}
+}
+print(programming_language)
+
+#loops in dict
+
+rogramming_language = {
+    "python":{"name":"python","versio":4.9,"use_case":[ 'ai,ml,webdev']}
+    ,"java":{"name":"java","versio":3.9,"use_case":[ 'ai,ml,webdev']}
+}
+for keys in programming_language.keys():#here also use values toget the value of dict
+ print(keys)
+
