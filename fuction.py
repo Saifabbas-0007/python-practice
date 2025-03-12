bfc6168cb88c7b07d970a331b7caedecd0991f40
Python practice <saifabbas0007@gmail.comgit config --global user.email saifabbas0007@gmail.com>
2025-03-12 23:07:58 +0530

    add me

--- fuction.py
+++ fuction.py
@@ -16,6 +16,32 @@ greet("saru")
 2_default arguments
 3_keyword arguments
 """
-def greet(name , city):
+#positional arguments
+def greet(name , city): 
     print(f'hello {name} welcome to the {city}')
 greet("saif","noida")
+
+#keywords arguments
+def greet(name , city): 
+    print(f'hello {name} welcome to the {city}')
+greet(name= "abbas",city ="noida")
+
+#default arguments
+def greet(name= "saru" , city= "ghaziabad"): 
+    print(f'hello {name} welcome to the {city}')
+greet() #if you pass the value here then it will run
+
+#return statments 
+def full_name (first_name,last_name):
+    name =f'{first_name} {last_name}'
+    return name
+    #print(name)
+variale = full_name("saru","saif")
+print(variale)
+
+'''
+characteristic
+function name must be meaninful
+mast be small in lenght
+avoid global variable
+'''
